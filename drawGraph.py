979f7944b8bad952a24bb14d5f368b1c61f99ddb
Jack <newcombej04@gmail.com>
2024-11-10 23:04:20 +0000

    Project 10/11/2024

--- drawGraph.py
+++ drawGraph.py
@@ -7,6 +7,6 @@ print(f"Created {len(graph1.nodes)} nodes and {len(graph1.connections)} connecti
 graph1.visualOutput()
 
 graph2 = graph.Graph()
-graph2.create(20, 1)
+graph2.create(15, 1)
 print(f"Created {len(graph2.nodes)} nodes and {len(graph2.connections)} connections in a {10}x{10} lattice.")
 graph2.visualOutput()
