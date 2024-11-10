093d671b761c5d86e438e845ab488e7176eb638c
Jack <newcombej04@gmail.com>
2024-11-10 23:00:34 +0000

    Project 10/11/2024

--- drawGraph.py
+++ drawGraph.py
@@ -2,7 +2,7 @@
 import graph
 
 graph1 = graph.Graph()
-graph1.create(20, 0)
+graph1.create(15, 0)
 print(f"Created {len(graph1.nodes)} nodes and {len(graph1.connections)} connections in a {10}x{10} lattice.")
 graph1.visualOutput()
 
